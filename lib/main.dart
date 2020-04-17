ed6a431dde8246a33f3b80a2f8753fcb72ea0c5d
deyizhang <deyizhang2003@gmail.com>
2020-04-17 18:22:20 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        body: SafeArea(
+          child: Column(children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                child: Image.network("https://i.stack.imgur.com/34AD2.jpg"),
+                backgroundColor: Colors.white,
+                radius:30,
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
