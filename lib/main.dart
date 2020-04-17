2c571eb7a311fdb2ef8c75b3c366229b44acff39
deyizhang <deyizhang2003@gmail.com>
2020-04-17 19:14:51 -0400

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -46,18 +46,23 @@ class Myapp extends StatelessWidget {
                 ),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(15),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.add_circle,
-                  ),
-                  Text(
-                    "deyizhang2003@gmail.com",
-                    style: TextStyle(fontSize: 15, color: Colors.black),
-                  )
-                ],
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 37,
+                    ),
+                    Text(
+                      " deyizhang2003@gmail.com",
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    )
+                  ],
+                ),
               ),
             ),
           ]),
